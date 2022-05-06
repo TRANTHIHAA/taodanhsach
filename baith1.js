64183ae385b160a093df0993019ac3831b6f2f39
TRANTHIHAA <hat6434@gmail.com>
2022-05-06 14:31:45 +0700

    Add baith1.html

---
+++ baith1.js
@@ -0,0 +1,11 @@
+// function showMessage(){
+//     alert('xin chào');}
+// function showMessage(){
+//     document.getElementById('messageDev').innerHTML = 'Xin chào!';
+// }
+// function showMessage(){
+//     document.write('Xin chào');
+// }
+function showMessage(){
+    console.log('Xin chào');
+}
